be9489a8d9729c5ce4de1337e61d04c91470c65a
Daniel Juarez <75283901+DaesJuar03@users.noreply.github.com>
2022-11-02 23:10:08 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
 
-function mostrar_menu(){
+function mostrar_menu() {
 
     document.getElementById("move-content").classList.toggle('move-container-all');
     document.getElementById("show-menu").classList.toggle('show-lateral');
@@ -13,7 +13,7 @@ function mostrar_menu(){
 
 
 
-                            //Buscador de contenido
+//Buscador de contenido
 
 
 //Ejecutando funciones
@@ -21,27 +21,27 @@ document.getElementById("icon-search").addEventListener("click", mostrar_buscado
 document.getElementById("cover-ctn-search").addEventListener("click", ocultar_buscador);
 
 //Declarando variables
-bars_search =       document.getElementById("ctn-bars-search");
-cover_ctn_search =  document.getElementById("cover-ctn-search");
-inputSearch =       document.getElementById("inputSearch");
-box_search =        document.getElementById("box-search");
+bars_search = document.getElementById("ctn-bars-search");
+cover_ctn_search = document.getElementById("cover-ctn-search");
+inputSearch = document.getElementById("inputSearch");
+box_search = document.getElementById("box-search");
 
 
 //Funcion para mostrar el buscador
-function mostrar_buscador(){
+function mostrar_buscador() {
 
     bars_search.style.top = "80px";
     cover_ctn_search.style.display = "block";
     inputSearch.focus();
 
-    if (inputSearch.value === ""){
+    if (inputSearch.value === "") {
         box_search.style.display = "none";
     }
 
 }
 
 //Funcion para ocultar el buscador
-function ocultar_buscador(){
+function ocultar_buscador() {
 
     bars_search.style.top = "-10px";
     cover_ctn_search.style.display = "none";
@@ -55,28 +55,28 @@ function ocultar_buscador(){
 
 document.getElementById("inputSearch").addEventListener("keyup", buscador_interno);
 
-function buscador_interno(){
+function buscador_interno() {
 
 
     filter = inputSearch.value.toUpperCase();
     li = box_search.getElementsByTagName("li");
 
     //Recorriendo elementos a filtrar mediante los "li"
-    for (i = 0; i < li.length; i++){
+    for (i = 0; i < li.length; i++) {
 
         a = li[i].getElementsByTagName("a")[0];
         textValue = a.textContent || a.innerText;
 
-        if(textValue.toUpperCase().indexOf(filter) > -1){
+        if (textValue.toUpperCase().indexOf(filter) > -1) {
 
             li[i].style.display = "";
             box_search.style.display = "block";
 
-            if (inputSearch.value === ""){
+            if (inputSearch.value === "") {
                 box_search.style.display = "none";
             }
 
-        }else{
+        } else {
             li[i].style.display = "none";
         }
 
